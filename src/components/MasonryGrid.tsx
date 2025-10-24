4acbda734dc5fc99283891582b1e5a82e8c0294a
Kai Faust <kaifaust@gmail.com>
2025-10-24 12:28:12 -0600

    Refine portfolio spacing on mobile

--- src/components/MasonryGrid.tsx
+++ src/components/MasonryGrid.tsx
@@ -6,7 +6,7 @@ interface MasonryGridProps {
 
 export default function MasonryGrid({ children }: MasonryGridProps) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 px-8 max-w-7xl mx-auto pb-16">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:px-8 max-w-7xl mx-auto pb-16">
       {children}
     </div>
   );
